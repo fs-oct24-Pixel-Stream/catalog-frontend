9285c836c356c336db55644f0c0955ba93d1b67f
Halyna Kravchenko <108605949+minimal005@users.noreply.github.com>
2025-01-09 14:20:15 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './catalog-frontend/',
+  // base: './catalog-frontend/',
+  base: '/',
 });
