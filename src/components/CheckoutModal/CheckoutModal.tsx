38c4d693b4432194ba9babee4077d9062b1493a2
Maksym Seliutin <maxwell7k@gmail.com>
2025-01-11 11:01:48 +0200

    Checkpoint

--- src/components/CheckoutModal/CheckoutModal.tsx
+++ src/components/CheckoutModal/CheckoutModal.tsx
@@ -6,13 +6,40 @@ export const CheckoutModal = () => {
         <div className="modal-background"></div>
         <div className="modal-card">
           <header className="modal-card-head">
-            <p className="modal-card-title">Modal title</p>
+            <p className="modal-card-title">Confirm and complete your order</p>
             <button
-              className="delete"
+              className="delete modal_"
               aria-label="close"
             ></button>
           </header>
-          <section className="modal-card-body">{/* <!-- Content ... --> */}</section>
+          <section className="modal-card-body">
+            <table className="content">
+              <thead>
+                <tr>
+                  <th>product name</th>
+                  <th>puantity</th>
+                  <th>price</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Товар 1</td>
+                  <td>5</td>
+                  <td>100.</td>
+                </tr>
+                <tr>
+                  <td>Товар 2</td>
+                  <td>3</td>
+                  <td>200.</td>
+                </tr>
+                <tr>
+                  <td>Товар 3</td>
+                  <td>10</td>
+                  <td>50.</td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
           <footer className="modal-card-foot">
             <div className="buttons">
               <button className="choice-button choice-button-confirm">Confirm</button>
