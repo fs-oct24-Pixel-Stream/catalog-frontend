fd063527b6a50cf773b4767b3570a1e0189b41e0
Maksym Seliutin <maxwell7k@gmail.com>
2025-01-11 21:55:19 +0200

    feat: implemented modal succsses window

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -3,12 +3,14 @@ import { CartItem } from '../../components/CartItem/CartItem';
 import { useState } from 'react';
 import { CheckoutModal } from '../../components/CheckoutModal/CheckoutModal';
 import './CartPage.scss';
-
+import { ModalMessage } from '../../components/ModalMessage/ModalMessage';
 export const CartPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isPurchased, setIsPurchased] = useState(false);
 
   const handleOpenModal = (param: boolean) => {
     setIsModalOpen(param);
+    setIsPurchased(isModalOpen ? true : false);
   };
 
   const cart = useAppSelector((state) => state.cart.cart);
@@ -35,6 +37,7 @@ export const CartPage = () => {
           setIsModalOpen={setIsModalOpen}
         />
       )}
+      {isPurchased && <ModalMessage setIsPurchased={setIsPurchased} />}
     </>
   );
 };
