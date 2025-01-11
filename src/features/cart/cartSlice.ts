510813a0de21a32fe164faecf5b0a4d904be0c99
Halyna Kravchenko <108605949+minimal005@users.noreply.github.com>
2025-01-11 18:22:27 +0200

    added cartSlice

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -1,8 +1,9 @@
-/* eslint-disable */
-import { createSlice } from '@reduxjs/toolkit';
-// import { ProductCardType } from '../../utils/types/ProductCardType';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { CartType } from '../../utils/types/CartType';
+import { ProductCardType } from '../../utils/types/ProductCardType';
+
 type InitialState = {
-  productsCart: { [key: number]: number } | {};
+  productsCart: Record<number, CartType>;
 };
 
 const initialState: InitialState = {
@@ -12,5 +13,50 @@ const initialState: InitialState = {
 export const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {},
+  reducers: {
+    addProductToCart: (state, { payload }: PayloadAction<ProductCardType>) => {
+      if (state.productsCart[payload.id]) {
+        state.productsCart[payload.id].quantity += 1;
+      } else {
+        state.productsCart[payload.id] = {
+          ...payload,
+          quantity: 1,
+          isBuy: true,
+        };
+      }
+    },
+
+    deleteProductFromCart: (state, { payload }: PayloadAction<number>) => {
+      //payload is product.id
+      delete state.productsCart[payload];
+    },
+
+    isBuy: (state, { payload }: PayloadAction<number>) => {
+      //payload is product.id
+      if (state.productsCart[payload]) {
+        state.productsCart[payload].isBuy = !state.productsCart[payload].isBuy;
+      }
+    },
+
+    incrementProduct: (state, { payload }: PayloadAction<number>) => {
+      //payload is product.id
+      if (state.productsCart[payload]) {
+        state.productsCart[payload].quantity += 1;
+      }
+    },
+    decrementProduct: (state, { payload }: PayloadAction<number>) => {
+      //payload is product.id
+      if (state.productsCart[payload] && state.productsCart[payload].quantity > 1) {
+        state.productsCart[payload].quantity -= 1;
+      }
+    },
+  },
 });
+
+export const {
+  addProductToCart,
+  deleteProductFromCart,
+  isBuy,
+  incrementProduct,
+  decrementProduct,
+} = cartSlice.actions;
