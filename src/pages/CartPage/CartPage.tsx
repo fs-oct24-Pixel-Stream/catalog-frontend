2bf92c71bc3786f67349fb03e268ed96c3207b72
Maksym Seliutin <maxwell7k@gmail.com>
2025-01-11 12:49:33 +0200

    fix: small changes in checkout.tsx

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -8,6 +8,80 @@ export const CartPage = () => {
     setIsModalOpen(param);
   };
 
+  const products = [
+    {
+      id: 1,
+      name: 'Product 1',
+      quantity: 2,
+      price: 100,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      quantity: 1,
+      price: 50,
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      quantity: 3,
+      price: 150,
+    },
+    {
+      id: 4,
+      name: 'Product 4',
+      quantity: 4,
+      price: 200,
+    },
+    {
+      id: 5,
+      name: 'Product 5',
+      quantity: 5,
+      price: 250,
+    },
+    {
+      id: 6,
+      name: 'Product 6',
+      quantity: 6,
+      price: 300,
+    },
+    {
+      id: 6,
+      name: 'Product 6',
+      quantity: 6,
+      price: 300,
+    },
+    {
+      id: 6,
+      name: 'Product 6',
+      quantity: 6,
+      price: 300,
+    },
+    {
+      id: 6,
+      name: 'Product 6',
+      quantity: 6,
+      price: 300,
+    },
+    {
+      id: 6,
+      name: 'Product 6',
+      quantity: 6,
+      price: 300,
+    },
+    {
+      id: 6,
+      name: 'Product 6',
+      quantity: 6,
+      price: 300,
+    },
+    {
+      id: 6,
+      name: 'Product 6',
+      quantity: 6,
+      price: 300,
+    },
+  ];
   return (
     <>
       <div>CartPage</div>
@@ -17,7 +91,12 @@ export const CartPage = () => {
       >
         Checkout
       </button>
-      {isModalOpen && <CheckoutModal setIsModalOpen={setIsModalOpen} />}
+      {isModalOpen && (
+        <CheckoutModal
+          productsList={products}
+          setIsModalOpen={setIsModalOpen}
+        />
+      )}
     </>
   );
 };
