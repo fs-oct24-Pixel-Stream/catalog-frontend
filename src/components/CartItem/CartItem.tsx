9943a4c2d5cead0e77029bb28be60c611ce99e90
Maksym Seliutin <maxwell7k@gmail.com>
2025-01-12 22:52:39 +0200

    checkpoint

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -27,7 +27,6 @@ export const CartItem: React.FC<Props> = ({ product }) => {
   };
 
   const cart = useAppSelector((state) => state.cart.cart);
-  console.log(cart);
   return (
     <div className="is-flex cart-item">
       <div className="is-flex is-justify-content-space-between is-align-items-center cart-item__header">
@@ -42,7 +41,7 @@ export const CartItem: React.FC<Props> = ({ product }) => {
             className="cart-item__image-url"
           >
             <img
-              src="https://s3-alpha-sig.figma.com/img/4036/d779/98ea87fdcb4fe9b1d62dd9629b0b820f?Expires=1737331200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dW0Qq4841iTFau5uqnnAA8anxDFnDtFNxKGq92Uq9fGG5jTIO5TEVGYRN0rnS7B-qgUQVa44SikVlxvAs7d3wEGY7kaJqdbmC3fxWDww~nj4IDEis1AMBLdxh49jt~TncmMVVfioxN6IgQdC3E0j7L8mo4V8UBCyhjsmuG60QNIYRTtErWml4CVhLLNfOIQEluMfilexyX9BmS3QT48rso6j-EkWHgzY5z7Za6YYXPo0KPrNT7AcQ8wW6FTsIXq3TR6BHl-Xjy0WjGWeUPNaPFVZSDyh5Bti5ZNnAIKMIo5pBRyop78~e40vaGawtIY~aJ4ZJWwYiidxAos1038RNQ__"
+              src={product.image}
               className=" cart-item__image"
               alt="Placeholder image"
             />
@@ -72,7 +71,7 @@ export const CartItem: React.FC<Props> = ({ product }) => {
           />
         </div>
 
-        <p className="cart-item__price">$999</p>
+        <p className="cart-item__price">${product.price * quantity}</p>
       </div>
     </div>
   );
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,10 +1,9 @@
 import { useAppSelector } from '../../app/hooks';
 import { CartItem } from '../../components/CartItem/CartItem';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { CheckoutModal } from '../../components/CheckoutModal/CheckoutModal';
 import './CartPage.scss';
 import { ModalMessage } from '../../components/ModalMessage/ModalMessage';
-import { CartItem } from '../../components/CartItem/CartItem';
 export const CartPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isPurchased, setIsPurchased] = useState(false);
@@ -29,28 +28,62 @@ export const CartPage = () => {
   const cart = useAppSelector((state) => state.cart.cart);
   console.log(cart);
 
+
+  const totalPrice = useMemo(() => {
+    return cart.reduce((acc, product) => {
+      return acc + product.price * product.quantity;
+    }, 0);
+  }, [cart]);
+  
+  const totalQuantity = useMemo(() => {
+    return cart.reduce((acc, product) => {
+      return acc + product.quantity;
+    }, 0);
+  }, [cart]);
+
+  
+
+  
   return (
     <>
-      {!!cart.length &&
-        cart.map((product) => (
-          <CartItem
-            key={product.id}
-            product={product}
-          />
-        ))}
-      <button
-        className="button main-button"
-        onClick={() => handleOpenModal(true)}
-      >
-        Checkout
-      </button>
+      <div className="cart _container">
+        <div className="cart-block">
+          <div className="cart-block__list">
+            {!!cart.length &&
+              cart.map((product) => (
+                <CartItem
+                  key={product.id}
+                  product={product}
+                />
+              ))}
+          </div>
+
+          {!!cart.length && (
+            <div className="checkout-block">
+              <h2 className="checkout-block__header-title">${totalPrice}</h2>
+            <p className="checkout-block__products-quantity">for {totalQuantity} products</p>
+            <button
+              className="button main-button"
+              onClick={() => handleOpenModal()}
+            >
+              Checkout
+            </button>
+          </div>
+          )}
+        </div>
+      </div>
       {isModalOpen && (
         <CheckoutModal
-          productsList={products}
-          setIsModalOpen={setIsModalOpen}
+          productsList={cart}
+          handleCloseModal={handleCloseModal}
         />
       )}
-      {isPurchased && <ModalMessage setIsPurchased={setIsPurchased} />}
+      {isPurchased && (
+        <ModalMessage
+          text={succsessModalWindowText}
+          setIsPurchased={setIsPurchased}
+        />
+      )}
     </>
   );
 };
