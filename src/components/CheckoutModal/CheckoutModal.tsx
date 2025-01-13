330d88a0583371a2dffe45ba8653ff84a95754e2
Maksym Seliutin <maxwell7k@gmail.com>
2025-01-13 14:16:21 +0200

    feat: implemented chechout-cartItem, fixed styles

--- src/components/CheckoutModal/CheckoutModal.tsx
+++ src/components/CheckoutModal/CheckoutModal.tsx
@@ -1,4 +1,5 @@
 import { CartType } from '../../utils/types/CartType';
+import { CheckoutCartItem } from '../CheckoutCartItem/CheckoutCartItem';
 import './CheckoutModal.scss';
 
 type Props = {
@@ -14,56 +15,67 @@ export const CheckoutModal: React.FC<Props> = (props) => {
         <div className="modal-background"></div>
         <div className="modal-card">
           <header className="modal-card-head">
-            <p className="modal-card-title">Confirm and complete your order</p>
+            <p className="modal-card-title">Confirm your order</p>
           </header>
           <section className="modal-card-body">
-            <table className="content">
-              <thead className="table-header">
-                <tr className="table-header__row">
-                  <th>product name</th>
-                  <th>quantity</th>
-                  <th>price</th>
-                </tr>
-              </thead>
-              <tbody className="table-body">
-                {productsList.map((product) => {
-                  return (
-                    <tr
-                      key={product.id}
-                      className="table-body__row"
-                    >
-                      <td>{product.name}</td>
-                      <td>{product.quantity}</td>
-                      <td>{product.price}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+            {productsList.map((product) => {
+              return (
+                <CheckoutCartItem
+                  key={product.id}
+                  quantity={product.quantity}
+                  product={product}
+                />
+              );
+            })}
           </section>
+
+
           <footer className="modal-card-foot">
-            <div className="price-block">
-              <p>Total price:$/₴0000</p>
-            </div>
+
             <div className="buttons">
-              <button
-                className="choice-button choice-button-confirm"
-                onClick={() => {
-                  handleCloseModal('succsess');
-                }}
-              >
-                Confirm
-              </button>
-              <button
+
+              <div className="confirm">
+
+                <div className="price-block">
+
+                  <p>Total price:$/₴0000</p>
+
+                </div>
+
+                <button
+                  
+                  className="choice-button choice-button-confirm"
+                  onClick={() => {
+                    handleCloseModal('succsess');
+                  }}
+                >
+                  Confirm
+                </button>
+
+
+              </div>
+
+              <div className='cancel'>
+
+                <button
                 className="choice-button choice-button-cancel"
                 onClick={() => {
                   handleCloseModal('close');
                 }}
               >
                 Cancel
-              </button>
+                </button>
+                
+              </div>
+              
             </div>
+
           </footer>
+          
+
+
+
+
         </div>
       </div>
     </>
