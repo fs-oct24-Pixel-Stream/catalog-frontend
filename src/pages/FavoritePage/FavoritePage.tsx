ecd01cd166249da1cc5eccbffbe2fd883f212678
Maksym Seliutin <maxwell7k@gmail.com>
2025-01-15 01:04:15 +0200

    feat: almost done with Favorites Page

--- src/pages/FavoritePage/FavoritePage.tsx
+++ src/pages/FavoritePage/FavoritePage.tsx
@@ -1,4 +1,31 @@
+import { useAppSelector } from '../../app/hooks';
+import { ProductCard } from '../../components/ProductCard';
 import './FavoritePage.scss';
 export const FavoritePage = () => {
-  return <div>Favorite</div>;
+  const products = useAppSelector((state) => state.favorities.products);
+  const isFavoritesEmpty = !!products.length;
+  const quantity = products.length;
+  return (
+    <>
+      <div className="favorites _container">
+        <h1 className="titleMain">Favorites</h1>
+        {!isFavoritesEmpty ?
+          <div className="favorites__background">
+            <h2 className="favorites__background__title">Your favorites are empty</h2>
+          </div>
+        : <>
+            <p className="favorites__items-quantity">{quantity} items</p>
+            <div className="favorites__list productList">
+              {products.map((product) => (
+                <ProductCard
+                  key={product.id}
+                  product={product}
+                />
+              ))}
+            </div>
+          </>
+        }
+      </div>
+    </>
+  );
 };
