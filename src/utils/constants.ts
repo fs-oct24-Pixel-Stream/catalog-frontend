10604801e381ac7ff7adebf74f37469438e315e9
Maksym Seliutin <maxwell7k@gmail.com>
2025-01-15 20:19:11 +0200

    init

---
+++ src/utils/constants.ts
@@ -0,0 +1,49 @@
+import { CreatorType } from './types/CreatorType';
+
+export const creators: CreatorType[] = [
+  {
+    id: 1,
+    name: 'Maksym',
+    surname: 'Seliutin',
+    linkedinLink: 'www.linkedin.com/in/maksym-seliutin',
+    githubLink: 'https://github.com/MakksymSly',
+    avatar: './img/creators/maksym.jpg',
+    hobby: 'Frontend developer',
+  },
+  {
+    id: 2,
+    name: 'Maksym',
+    surname: 'Seliutin',
+    linkedinLink: 'www.linkedin.com/in/maksym-seliutin',
+    githubLink: 'https://github.com/MakksymSly',
+    avatar: './img/creators/maksym.jpg',
+    hobby: 'Frontend developer',
+  },
+  {
+    id: 3,
+    name: 'Maksym',
+    surname: 'Seliutin',
+    linkedinLink: 'www.linkedin.com/in/maksym-seliutin',
+    githubLink: 'https://github.com/MakksymSly',
+    avatar: './img/creators/maksym.jpg',
+    hobby: 'Frontend developer',
+  },
+  {
+    id: 4,
+    name: 'Maksym',
+    surname: 'Seliutin',
+    linkedinLink: 'www.linkedin.com/in/maksym-seliutin',
+    githubLink: 'https://github.com/MakksymSly',
+    avatar: './img/creators/maksym.jpg',
+    hobby: 'Frontend developer',
+  },
+  {
+    id: 5,
+    name: 'Maksym',
+    surname: 'Seliutin',
+    linkedinLink: 'www.linkedin.com/in/maksym-seliutin',
+    githubLink: 'https://github.com/MakksymSly',
+    avatar: './img/creators/maksym.jpg',
+    hobby: 'Frontend developer',
+  },
+];
