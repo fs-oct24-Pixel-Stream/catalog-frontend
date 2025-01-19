ecd8dc9a2a50538d3d1fb6ca3ade4da4e6d06ad0
Andriy Mekshun <andreymekshun@gmail.com>
2025-01-19 22:57:56 +0200

    feat: implemented skeleton

---
+++ src/components/Skeletons/ProductCardSkeleton/ProductCardSkeleton.tsx
@@ -0,0 +1,80 @@
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { useAppSelector } from '../../../app/hooks';
+
+export const ProductCardSkeleton = () => {
+  const isThemeDark = useAppSelector((state) => state.theme.theme) === 'dark';
+
+  return (
+    <SkeletonTheme
+      baseColor={isThemeDark ? '#333' : '#e0e0e0'}
+      highlightColor={isThemeDark ? '#444' : '#f5f5f5'}
+    >
+      <div className="is-flex is-flex-direction-column is-justify-content-space-between product-card">
+        <div className="product-card__image">
+          <Skeleton height={190} />
+        </div>
+
+        <div className="product-card__title">
+          <Skeleton
+            width="80%"
+            height={40}
+          />
+        </div>
+
+        <div className="is-flex product-card__price">
+          <Skeleton
+            width={50}
+            height={30}
+          />
+        </div>
+
+        <div className="product-card__line"></div>
+
+        <div className="is-flex is-align-items-center is-justify-content-space-between product-card__descriptions">
+          <Skeleton
+            width={50}
+            height={14}
+          />
+          <Skeleton
+            width={30}
+            height={14}
+          />
+        </div>
+
+        <div className="is-flex is-align-items-center is-justify-content-space-between product-card__descriptions">
+          <Skeleton
+            width={50}
+            height={14}
+          />
+          <Skeleton
+            width={30}
+            height={14}
+          />
+        </div>
+
+        <div className="is-flex is-align-items-center is-justify-content-space-between product-card__descriptions">
+          <Skeleton
+            width={50}
+            height={14}
+          />
+          <Skeleton
+            width={30}
+            height={14}
+          />
+        </div>
+
+        <div className="is-flex is-justify-content-space-between product-card__buttons">
+          <Skeleton
+            width={175}
+            height={40}
+          />
+          <Skeleton
+            width={40}
+            height={40}
+          />
+        </div>
+      </div>
+    </SkeletonTheme>
+  );
+};
